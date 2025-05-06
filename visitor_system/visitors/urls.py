63fe316d2cc7f1cb04e2f8b98c5b418967567ddf
Sagat Akimbay <maroccocombo@gmail.com>
2025-05-06 18:13:53 +0500

    Update urls.py

--- visitor_system/visitors/urls.py
+++ visitor_system/visitors/urls.py
@@ -3,16 +3,16 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # --- Новый единый URL для регистрации ---
+    # --- единый URL для регистрации ---
     # path('register/', views.unified_register_visit_view, name='unified_register_visit'),
     # ------------------------------------
     
     path('register/', views.register_guest_view, name='register_guest'),
-    # --- Новый путь для регистрации студентов/абитуриентов ---
+    # --- путь для регистрации студентов/абитуриентов ---
     path('register/student/', views.register_student_visit_view, name='register_student_visit'),
     #path('register-group/', views.register_group_visit_view, name='register_group_visit'),
     
-    # --- Новый URL для настройки профиля ---
+    # --- URL для настройки профиля ---
     path('profile/setup/', views.profile_setup_view, name='profile_setup'),
     
     path('current/', views.current_guests_view, name='current_guests'),
@@ -20,8 +20,7 @@ urlpatterns = [
     path('history/detail/<int:visit_id>/', views.visit_detail_view, name='visit_detail'),
     path('history/student/detail/<int:visit_id>/', views.student_visit_detail_view, name='student_visit_detail'),
     
-    # --- Новые пути для Check-in и Cancel ---
-    # Мы передаем 'visit_kind' как параметр в view
+    # --- пути для Check-in и Cancel ---
     path('checkin/<str:visit_kind>/<int:visit_id>/', views.check_in_visit, name='check_in_visit'),
     path('cancel/<str:visit_kind>/<int:visit_id>/', views.cancel_visit, name='cancel_visit'),
     # -----------------------------------------
@@ -34,16 +33,16 @@ urlpatterns = [
     path('dashboard/employee/', views.employee_dashboard_view, name='employee_dashboard'),
     path('dashboard/admin/', views.admin_dashboard_view, name='admin_dashboard'),
     path('ajax/employee-autocomplete/', views.employee_autocomplete_view, name='employee_autocomplete'),
-    # --- Новый путь для статистики ---
+    # --- путь для статистики ---
     path('statistics/', views.visit_statistics_view, name='visit_statistics'),
     
-    # --- Новый путь для экспорта в Excel ---
+    # --- путь для экспорта в Excel ---
     path('history/export/xlsx/', views.export_visits_xlsx, name='export_visits_xlsx'),
     
     path('ajax/employee-autocomplete/', views.employee_autocomplete_view, name='employee_autocomplete'),
-    # --- Новый URL для получения деталей сотрудника ---
+    # --- URL для получения деталей сотрудника ---
     path('ajax/employee-details/<int:user_id>/', views.get_employee_details_view, name='get_employee_details'),
-    # --- Новый URL для деталей департамента ---
+    # --- URL для деталей департамента ---
     path('ajax/department-details/', views.get_department_details_view, name='get_department_details'),
     
 ]
